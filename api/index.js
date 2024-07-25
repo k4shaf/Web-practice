eec1143dad3b9923acb0167959cf0ed822fa0007
K4shaf <naqvikashaf41@gmail.com>
2024-07-25 19:15:28 +0500

    Changed path urls

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 export default function handler(req, res) {
-  const filePath = join(process.cwd(), 'views', 'index.html');
+  const filePath = join(process.cwd(), 'public', 'index.html');
   const fileContent = readFileSync(filePath, 'utf8');
   res.status(200).send(fileContent);
 }
